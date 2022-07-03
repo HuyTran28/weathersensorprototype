ac346fd62c7b78229e0bd105da8d586b55bced87
Tran Cam Huy <huycamhuy@gmail.com>
2022-07-03 16:12:53 +0700

    Update weathersensorprototype.ino

--- weathersensorprototype.ino
+++ weathersensorprototype.ino
@@ -6,6 +6,7 @@
 
 #define I2C_ADDRESS       0x44
 #define ML8511_ANALOGPIN     A0
+#define MKES12_PIN           A1
 
 SHT85 sht;
 ML8511 ml8511(ML8511_ANALOGPIN);
@@ -17,9 +18,12 @@ void getHumidTemp()
   
   float temp = sht.getTemperature();
   float humid = sht.getHumidity();
-  
+
+  Serial.print("humidity:");
   Serial.print(humid,4);
   Serial.print("%\t");
+
+  Serial.print("temperature:");
   Serial.print(temp,4);
   Serial.println("℃");
 }
@@ -27,14 +31,30 @@ void getHumidTemp()
 void getUVData()
 {
   float UV = ml8511.getUV();
-  
+
+  Serial.print("UV index:");
   Serial.print(UV,4);
   Serial.println("mW cm^2"); 
 }
 
+void getPM_2_5()
+{
+  int PM_2_5 = pms7003.getPM_2_5();
+
+  Serial.print("PM2.5:");
+  Serial.println(PM_2_5);
+}
+
+void getRainWater()
+{
+  int RainWater = analogRead(MKES12_PIN);
+  
+  Serial.println(RainWater);
+}
+
 void setup() 
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   Wire.begin();
   
   sht.begin(I2C_ADDRESS);
@@ -43,7 +63,5 @@ void setup()
 
 void loop() 
 {
-  getHumidTemp();
-  getUVData();
   delay(2000);
 }
